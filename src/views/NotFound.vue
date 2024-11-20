014d8dbd7d3f0a91b98ca90049bf5919f6dafeaf
zhang <2135978695@qq.com>
2024-11-21 01:23:48 +0800

    2024-11-21-u

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -25,11 +25,21 @@ onUnmounted(()=>{
     <div class="com-404">
         <h1 style="text-align:center;;font-size: 60px;font-weight: bolder">4 0 4</h1>
         <h2 style="text-align:center;">{{ number }}</h2>
+        <el-button round>
+            <router-link to="/">返回首页</router-link>
+        </el-button>
     </div>
 </template>
 
 <style>
 .com-404{
     margin: 40vh auto;
+    width: 500px;
+    padding: 50px;
+    border-radius: 20px;
+    background: antiquewhite;
+
+    display: flex;
+    flex-direction: column;
 }
 </style>
